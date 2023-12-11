c6bb5067867b6db5ff05b6e8d89dce855294e590
Wayne Ellis <wayne@wayneellis.co.uk>
2023-12-11 06:36:32 +0000

    Day 11 - tidy up

--- src/main/kotlin/Day11.kt
+++ src/main/kotlin/Day11.kt
@@ -12,7 +12,11 @@ class Day11 {
     data class Point(val x:Int, val y:Int)
     private fun isSpaceBetween(start:Int, end:Int, toCompare: Int) = toCompare in min(start, end) .. max(start, end)
 
-    fun puzzle1() {
+    fun puzzle1() { solve(2).printAnswer() }
+    fun puzzle2() { solve(1_000_000).printAnswer() }
+
+    // almost identical solutions to p1 / p2, so tidy up with a shared function
+    private fun solve(multiplier: Int) : Long {
         val universe = file.readLines().map { it.toMutableList() }.toMutableList()
         val emptyRows = (0..< universe.size).filter { universe[it].all { it == '.' } }
         val emptyCols = (0..< universe[0].size).filter { col -> (0..< universe.size).map { r -> universe[r][col] }.all { it == '.' } }
@@ -22,31 +26,12 @@ class Day11 {
         }
 
         val pairs = galaxies.combinations(2)
-        pairs.sumOf {
+        return pairs.sumOf {
             val distance = abs(it[0].x-it[1].x) + abs(it[0].y-it[1].y)
             val rowSpace = emptyRows.count { row-> isSpaceBetween(it[0].y, it[1].y, row) }
             val colSpace = emptyCols.count { col -> isSpaceBetween(it[0].x, it[1].x, col)  }
 
-            distance + rowSpace + colSpace
-        }.printAnswer()
-    }
-
-    fun puzzle2() {
-        val universe = file.readLines().map { it.toMutableList() }.toMutableList()
-        val emptyRows = (0..< universe.size).filter { universe[it].all { it == '.' } }
-        val emptyCols = (0..< universe[0].size).filter { col -> (0..< universe.size).map { r -> universe[r][col] }.all { it == '.' } }
-
-        val galaxies = universe.flatMapIndexed  { row, chars ->
-            chars.mapIndexedNotNull { col, c -> if (c == '#') Point(col, row) else null }
+            distance + rowSpace*(multiplier-1L) + colSpace*(multiplier-1L)
         }
-
-        val pairs = galaxies.combinations(2)
-        pairs.sumOf {
-            val distance = abs(it[0].x-it[1].x) + abs(it[0].y-it[1].y)
-            val rowSpace = emptyRows.count { row-> isSpaceBetween(it[0].y, it[1].y, row) }
-            val colSpace = emptyCols.count { col -> isSpaceBetween(it[0].x, it[1].x, col)  }
-
-            distance + rowSpace*999_999L + colSpace*999_999L
-        }.printAnswer()
     }
 }
